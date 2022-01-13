8214eff9814a88293bbe4c97a82a2875e2425fe9
Domirando <maisiedev@gmail.com>
2022-01-13 05:14:02 +0500

    adding core files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, Move, MoveIn, MoveOut, Sticky, StickyIn, ZoomIn } from "react-scroll-motion";
+import {Fragment} from "react";
+
+const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+const FadeUp = batch(Fade(), Move(), Sticky());
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Fragment>
+          <ScrollContainer>
+              <ScrollPage page={0}>
+                  <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
+                      <h1 className="bg-black text-white p-2 text-3xl font-bold underline">Hello world!</h1>
+                  </Animator>
+              </ScrollPage>
+              <ScrollPage page={1}>
+                  <Animator animation={ZoomInScrollOut}>
+                      <p className='text-2xl'>I'm FadeUpScrollOut ✨ made by <span className='bg-black text-white p-2 underline font-bold'>Domirando</span></p>
+                  </Animator>
+              </ScrollPage>
+              <ScrollPage page={2}>
+                  <Animator animation={FadeUp}>
+                      <span className='text-3xl'>I'm FadeUp ⛅️</span>
+                  </Animator>
+              </ScrollPage>
+              <ScrollPage page={3}>
+                  <div className='flex justify-center items-center h-full' >
+      <span className='text-3xl'>
+        <Animator animation={MoveIn(-1000, 0)}>Hi there 👋🏻</Animator>
+        <Animator animation={MoveIn(1000, 0)}>Nice to see you 🙋🏻‍♀️</Animator>- I'm Domirando -
+        <Animator animation={MoveOut(1000, 0)}>Good bye ✋🏻</Animator>
+        <Animator animation={MoveOut(-1000, 0)}>See you 💛</Animator>
+      </span>
+                  </div>
+              </ScrollPage>
+              <ScrollPage page={4}>
+                  <Animator animation={batch(Fade(), Sticky())}>
+                      <span className='text-3xl'>Done</span>
+                      <br/>
+                      <span className='text-3xl'>
+        There's FadeAnimation, MoveAnimation, StickyAnimation, ZoomAnimation
+      </span>
+                  </Animator>
+              </ScrollPage>
+          </ScrollContainer>
+      </Fragment>
+    );
 }
 
 export default App;
