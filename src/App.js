2229e94fa42b41d8218e77a15fa5f96508ddd0e9
Domirando <vohidjonovnamaftuna@gmail.com>
2025-07-03 13:37:52 +0500

    feat: medium account added

--- src/App.js
+++ src/App.js
@@ -27,9 +27,9 @@ function App() {
             <span className="text-3xl flex flex-col gap-3">
               <Animator animation={MoveIn(-1000, 0)}>You can find all my mini projects on my <a href='https://github.com/Domirando' className='underline text-white bg-black p-0.5'> GitHub</a> profile</Animator>
               <Animator animation={MoveIn(1000, 0)}>
-                You can read more about me in my <a href='https://www.linkedin.com/in/maftuna-vohidjonovna/' className='underline text-white bg-black p-0.5'>Linkedin</a> profile
+               You can read more about me in my <a href='https://www.linkedin.com/in/maftuna-vohidjonovna/' className='underline text-white bg-black p-0.5'>Linkedin</a> <a href='https://t.me/domirandos' className='underline text-white bg-black p-0.5'>Telegram</a> profiles
               </Animator>
-              <Animator animation={MoveIn(-1000, 0)}>And don't forget checking out my little <a href='https://domirandos-blog.vercel.app/' className='underline text-white bg-black p-0.5'> BLOG</a> :)</Animator>
+              <Animator animation={MoveIn(-1000, 0)}><i>And don't forget checking out my little <a href='https://domirandos-blog.vercel.app/' className='underline text-white bg-black p-0.5'> Personal Blog</a> <a href='https://medium.com/@Domirando' className='underline text-white bg-black p-0.5'> Medium</a> :)</i></Animator>
             </span>
           </div>
         </ScrollPage>
@@ -40,7 +40,7 @@ function App() {
             <br />
               <span className='text-xl w-7/12 flex justify-between mt-1'>
                <a href='mailto:maisiedev@gmail.com' className='underline text-white bg-black p-0.5'>Gmail</a>
-               <a href='https://t.me/domirandos' className='underline text-white bg-black p-0.5'>Linkedin</a>
+               <a href='https://www.linkedin.com/in/maftuna-vohidjonovna/' className='underline text-white bg-black p-0.5'>Linkedin</a>
               </span>
           </Animator>
         </ScrollPage>
